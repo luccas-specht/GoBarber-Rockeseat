a2d24974f64055f97458d0fdca8952f1d6fefbc4
luccas-specht <luccasspecht70@gmail.com>
2020-11-01 14:33:55 -0300

    adicionando app

---
+++ appgobarber/src/pages/register/register.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './style'
+
+const Register = () => {
+    return (
+        <Container />
+    );
+}
+
+export { Register }
---
+++ appgobarber/src/routes/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import { Login, Register } from '../pages'
+
+const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => (
+    <Auth.Navigator
+        screenOptions={{
+            headerShown: false,
+            cardStyle: { backgroundColor: '#312e38' }
+        }}>
+        <Auth.Screen name='Login' component={Login} />
+        <Auth.Screen name='Register' component={Register} />
+    </Auth.Navigator>
+);
+
+
+export { AuthRoutes }
