0d1c2ee6b0d861a820a748458cf126582bb6a4f6
luccas-specht <luccasspecht70@gmail.com>
2020-12-08 18:46:10 -0300

    add

--- api/src/shared/infra/http/routes/index.ts
+++ api/src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { appointmentsRouter } from '@modules/appointments/infra/http/routes/appointments.route';
+import { providersRouter } from '@modules/appointments/infra/http/routes/provides.route';
 import { userRouter } from '@modules/users/infra/http/routes/users.route';
 import { authRouter } from '@modules/users/infra/http/routes/auth.route';
 import { passwordRoutes } from '@modules/users/infra/http/routes/password.route';
@@ -11,7 +12,8 @@ const routes = Router();
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', userRouter);
 routes.use('/auth', authRouter);
-routes.use('/password', passwordRoutes)
-routes.use('/profile', profileRouter)
+routes.use('/password', passwordRoutes);
+routes.use('/profile', profileRouter);
+routes.use('/providers', providersRouter)
 
 export { routes };
